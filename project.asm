b0cc12ba31be3e10e48e8261d9048935dc7bff45
Ilya Kachalskiy <21011996@mail.ru>
2015-05-29 16:41:22 +0300

    init asm

---
+++ project.asm
@@ -0,0 +1,33 @@
+default rel
+
+extern calloc
+extern printf
+extern free
+
+global parseExpr
+global parseSum
+global parseMultiplier
+global parseValue
+global next
+global dlina
+global constructor
+
+	struct Lexer
+.s		resq	1
+.cur	qword	1
+.length	qword	1
+	endstruc
+
+	
+next:
+parseValue:
+parseMultiplier:
+parseSum:
+parseExpr:
+dlina:
+constructor:
+
+
+	
+	
+
