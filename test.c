55f40971102a351b2df7abc161b0914a55b6afdc
Илья <21011996@mail.ru>
2015-06-10 01:59:48 +0300

    branch with double

--- test.c
+++ test.c
@@ -4,18 +4,18 @@
 #include <math.h>
 
 
-int testCalculate(int a, char const *s, int value){
+int testCalculate(int a, char const *s, double value){
 	int code = 0;
         int* codelink = &code;
-        int answer = calculate(s, codelink);
+        double answer = calculate(s, codelink);
         return value - answer;
 
 }
-void testresult(int a) {
+void testresult(double a) {
 	if (a == 0) {
 		printf("%s \n", "OK");
 	} else {
-		printf("%i \n", a);
+		printf("%f \n", a);
 	}
 }
 
@@ -26,10 +26,10 @@ int main() {
 	printf("%i \n", answer);
 	printf("%i \n", *codelink);
 */
-    testresult(testCalculate(2, "8*8/2%3", 2));
-    testresult(testCalculate(2, "(((1+2*2)))%(13-4)", 5));
-    testresult(testCalculate(2, "1000000*(((6-1-2-3)))", 0));
-    testresult(testCalculate(2, "44*6-((5)-(2*2))/2+3/3*((4))*(-333)", -1068));
-    testresult(testCalculate(2, "3*6/4-555%(7-8)+(((555)))/(((((99)))+(-94)))", 115));
+    testresult(testCalculate(2, "8.0*8.0/2.0%3.0", 2.0));
+//    testresult(testCalculate(2, "(((1+2*2)))%(13-4)", 5));
+  //  testresult(testCalculate(2, "1000000*(((6-1-2-3)))", 0));
+    //testresult(testCalculate(2, "44*6-((5)-(2*2))/2+3/3*((4))*(-333)", -1068));
+      //testresult(testCalculate(2, "3*6/4-555%(7-8)+(((555)))/(((((99)))+(-94)))", 115));
 
 }
